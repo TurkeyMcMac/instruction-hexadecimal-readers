19eab2753b4248a4700c878c5e60a30ef3aed75b
TurkeyMcMac <jwmhjwmh@gmail.com>
2019-01-19 15:13:02 -0500

    Rename library to reflect narrowed purpose

--- hex.c
+++ hex.c
@@ -13,18 +13,18 @@ static int read_u8(const char hex[2])
 	return (read_nibble(hex[0]) << 4) | read_nibble(hex[1]);
 }
 
-static int valid_type(int file_type, HEX_U8 type)
+static int valid_type(int file_type, IHR_U8 type)
 {
 	switch (type) {
-	case HEXR_DATA:
-	case HEXR_END_OF_FILE:
+	case IHRR_DATA:
+	case IHRR_END_OF_FILE:
 		return 1;
-	case HEXR_EXT_SEG_ADDR:
-	case HEXR_START_SEG_ADDR:
-		return file_type == HEXT_I16;
-	case HEXR_EXT_LIN_ADDR:
-	case HEXR_START_LIN_ADDR:
-		return file_type == HEXT_I32;
+	case IHRR_EXT_SEG_ADDR:
+	case IHRR_START_SEG_ADDR:
+		return file_type == IHRT_I16;
+	case IHRR_EXT_LIN_ADDR:
+	case IHRR_START_LIN_ADDR:
+		return file_type == IHRT_I32;
 	default:
 		return 0;
 	}
@@ -34,32 +34,32 @@ static int invalid_hex_error(const char *pair)
 {
 	switch (pair[0]) {
 	case '\n':
-		return -HEXE_INVALID_SIZE;
+		return -IHRE_INVALID_SIZE;
 	case '\r':
-		if (pair[1] == '\n') return -HEXE_INVALID_SIZE;
+		if (pair[1] == '\n') return -IHRE_INVALID_SIZE;
 		/* FALLTHROUGH */
 	default:
-		return -HEXE_NOT_HEX;
+		return -IHRE_NOT_HEX;
 	}
 }
 
-static int read_data(struct hex_record *rec, const char *hex)
+static int read_data(struct ihr_record *rec, const char *hex)
 {
-	HEX_U8 i;
+	IHR_U8 i;
 	int min_size;
 	switch (rec->type) {
-		case HEXR_EXT_SEG_ADDR:
-		case HEXR_EXT_LIN_ADDR:
+		case IHRR_EXT_SEG_ADDR:
+		case IHRR_EXT_LIN_ADDR:
 			min_size = 2;
 			break;
-		case HEXR_START_SEG_ADDR:
-		case HEXR_START_LIN_ADDR:
+		case IHRR_START_SEG_ADDR:
+		case IHRR_START_LIN_ADDR:
 			min_size = 4;
 			break;
 		default:
 			min_size = -1;
 	}
-	if ((int)rec->size < min_size) return -HEXE_INVALID_SIZE;
+	if ((int)rec->size < min_size) return -IHRE_INVALID_SIZE;
 	for (i = 0; i < rec->size; ++i) {
 		const char *pair = hex + i * 2;
 		int byte = read_u8(pair);
@@ -71,37 +71,37 @@ static int read_data(struct hex_record *rec, const char *hex)
 	return 0;
 }
 
-static void unionize_data(struct hex_record *rec)
+static void unionize_data(struct ihr_record *rec)
 {
-	HEX_U8 *data = rec->data.data;
+	IHR_U8 *data = rec->data.data;
 	switch (rec->type) {
-	case HEXR_DATA:
-	case HEXR_END_OF_FILE:
+	case IHRR_DATA:
+	case IHRR_END_OF_FILE:
 		break;
-	case HEXR_EXT_SEG_ADDR:
-	case HEXR_EXT_LIN_ADDR:
-		rec->data.base_addr = ((HEX_U16)data[0] << 8)
-			| (HEX_U16)data[1];
+	case IHRR_EXT_SEG_ADDR:
+	case IHRR_EXT_LIN_ADDR:
+		rec->data.base_addr = ((IHR_U16)data[0] << 8)
+			| (IHR_U16)data[1];
 		break;
-	case HEXR_START_SEG_ADDR:
-		rec->data.start.code_seg = ((HEX_U16)data[0] << 8)
-			| (HEX_U16)data[1];
-		rec->data.start.instr_ptr = ((HEX_U16)data[2] << 8)
-			| (HEX_U16)data[3];
+	case IHRR_START_SEG_ADDR:
+		rec->data.start.code_seg = ((IHR_U16)data[0] << 8)
+			| (IHR_U16)data[1];
+		rec->data.start.instr_ptr = ((IHR_U16)data[2] << 8)
+			| (IHR_U16)data[3];
 		break;
-	case HEXR_START_LIN_ADDR:
-		rec->data.ext_instr_ptr = ((HEX_U32)data[0] << 24)
-			| ((HEX_U32)data[1] << 16) | ((HEX_U32)data[2] << 8)
-			| (HEX_U32)data[3];
+	case IHRR_START_LIN_ADDR:
+		rec->data.ext_instr_ptr = ((IHR_U32)data[0] << 24)
+			| ((IHR_U32)data[1] << 16) | ((IHR_U32)data[2] << 8)
+			| (IHR_U32)data[3];
 		break;
 	}
 }
 
 
-static HEX_U8 calc_checksum(const struct hex_record *rec)
+static IHR_U8 calc_checksum(const struct ihr_record *rec)
 {
-	HEX_U8 checksum = 0;
-	HEX_U8 i = 0;
+	IHR_U8 checksum = 0;
+	IHR_U8 i = 0;
 	checksum += rec->size;
 	checksum += rec->addr >> 8;
 	checksum += rec->addr & 0xFF;
@@ -118,97 +118,97 @@ static HEX_U8 calc_checksum(const struct hex_record *rec)
 		if (feof((from))) { \
 			on_eof; \
 		} else { \
-			return -HEXE_IO_ERROR; \
+			return -IHRE_IO_ERROR; \
 		} \
 	}
 
-int hex_read(int file_type, FILE *from, struct hex_record *rec)
+int ihr_read(int file_type, FILE *from, struct ihr_record *rec)
 {
 	int err;
 	char buf[512];
 	int size, addr, type, checksum;
-	READ(from, 1, buf, return -HEXE_UNEXPECTED_EOF);
-	if (buf[0] != ':') return -HEXE_MISSING_COLON;
-	READ(from, 2, buf, return -HEXE_UNEXPECTED_EOF);
+	READ(from, 1, buf, return -IHRE_UNEXPECTED_EOF);
+	if (buf[0] != ':') return -IHRE_MISSING_COLON;
+	READ(from, 2, buf, return -IHRE_UNEXPECTED_EOF);
 	size = read_u8(buf);
 	rec->size = size;
-	if (size < 0) return -HEXE_NOT_HEX;
-	READ(from, 4, buf, return -HEXE_UNEXPECTED_EOF);
+	if (size < 0) return -IHRE_NOT_HEX;
+	READ(from, 4, buf, return -IHRE_UNEXPECTED_EOF);
 	addr = read_u8(buf);
-	if (addr < 0) return -HEXE_NOT_HEX;
+	if (addr < 0) return -IHRE_NOT_HEX;
 	rec->addr = addr << 8;
 	addr = read_u8(buf + 2);
-	if (addr < 0) return -HEXE_NOT_HEX;
+	if (addr < 0) return -IHRE_NOT_HEX;
 	rec->addr |= addr;
-	READ(from, 2, buf, return -HEXE_UNEXPECTED_EOF);
+	READ(from, 2, buf, return -IHRE_UNEXPECTED_EOF);
 	type = read_u8(buf);
 	rec->type = type;
-	if (type < 0) return -HEXE_NOT_HEX;
-	if (!valid_type(file_type, type)) return -HEXE_INVALID_TYPE;
-	READ(from, size * 2, buf, return -HEXE_INVALID_SIZE);
+	if (type < 0) return -IHRE_NOT_HEX;
+	if (!valid_type(file_type, type)) return -IHRE_INVALID_TYPE;
+	READ(from, size * 2, buf, return -IHRE_INVALID_SIZE);
 	err = read_data(rec, buf);
 	if (err < 0) return err;
-	READ(from, 2, buf, return -HEXE_UNEXPECTED_EOF);
+	READ(from, 2, buf, return -IHRE_UNEXPECTED_EOF);
 	checksum = read_u8(buf);
 	if (checksum < 0) return invalid_hex_error(buf);
 	READ(from, 1, buf,
-		if (rec->type == HEXR_END_OF_FILE) goto finish;
-		return -HEXE_UNEXPECTED_EOF;
+		if (rec->type == IHRR_END_OF_FILE) goto finish;
+		return -IHRE_UNEXPECTED_EOF;
 	);
 	switch (buf[0]) {
 	case '\n':
 		break;
 	case '\r':
-		READ(from, 1, buf, return -HEXE_EXPECTED_EOL);
+		READ(from, 1, buf, return -IHRE_EXPECTED_EOL);
 		if (buf[0] != '\n') {
-			return -HEXE_EXPECTED_EOL;
+			return -IHRE_EXPECTED_EOL;
 		}
 		break;
 	default:
-		return -HEXE_INVALID_SIZE;
+		return -IHRE_INVALID_SIZE;
 	}
 	if (fread(buf, 1, 1, from) != 1) {
 		if (feof(from)) {
-			if (rec->type != HEXR_END_OF_FILE) {
-				return -HEXE_UNEXPECTED_EOF;
+			if (rec->type != IHRR_END_OF_FILE) {
+				return -IHRE_UNEXPECTED_EOF;
 			}
 		} else {
-			return -HEXE_IO_ERROR;
+			return -IHRE_IO_ERROR;
 		}
-	} else if (rec->type == HEXR_END_OF_FILE) {
-		return -HEXE_MISSING_EOF;
+	} else if (rec->type == IHRR_END_OF_FILE) {
+		return -IHRE_MISSING_EOF;
 	} else {
 		ungetc(buf[0], from);
 	}
 finish:
-	if (checksum != calc_checksum(rec)) return -HEXE_INVALID_CHECKSUM;
+	if (checksum != calc_checksum(rec)) return -IHRE_INVALID_CHECKSUM;
 	unionize_data(rec);
 	return 0;
 }
 
-const char *hex_errstr(int code)
+const char *ihr_errstr(int code)
 {
 	if (code < 0) code *= -1;
 	switch (code) {
 	case 0:
 		return "Success";
-	case HEXE_EXPECTED_EOL:
+	case IHRE_EXPECTED_EOL:
 		return "Expected line ending";
-	case HEXE_INVALID_CHECKSUM:
+	case IHRE_INVALID_CHECKSUM:
 		return "Stored checksum does not match computed checksum";
-	case HEXE_INVALID_SIZE:
+	case IHRE_INVALID_SIZE:
 		return "Invalid byte count for record";
-	case HEXE_INVALID_TYPE:
+	case IHRE_INVALID_TYPE:
 		return "Invalid record type";
-	case HEXE_MISSING_COLON:
+	case IHRE_MISSING_COLON:
 		return "Expected ':' to begin a record";
-	case HEXE_MISSING_EOF:
+	case IHRE_MISSING_EOF:
 		return "Expected end-of-file";
-	case HEXE_NOT_HEX:
+	case IHRE_NOT_HEX:
 		return "Character pair is not a hexidecimal digit pair";
-	case HEXE_UNEXPECTED_EOF:
+	case IHRE_UNEXPECTED_EOF:
 		return "Unexpected end-of-file";
-	case HEXE_IO_ERROR:
+	case IHRE_IO_ERROR:
 		return "I/O error";
 	default:
 		return "Uknown error";
--- hex.h
+++ hex.h
@@ -1,57 +1,57 @@
-#ifndef HEX_INCLUDED
-#define HEX_INCLUDED
+#ifndef IHR_INCLUDED
+#define IHR_INCLUDED
 
 #include <stdio.h>
 #include <limits.h>
 
-typedef unsigned char HEX_U8;
-typedef unsigned short HEX_U16;
+typedef unsigned char IHR_U8;
+typedef unsigned short IHR_U16;
 typedef unsigned
 #if INT_MAX == SHORT_MAX /* int is 16 bits */
 	long
 #else
 	short
 #endif /* int is 16 bits */
-HEX_U32;
+IHR_U32;
 
-#define HEXT_I8		0
-#define HEXT_I16	1
-#define HEXT_I32	2
+#define IHRT_I8		0
+#define IHRT_I16	1
+#define IHRT_I32	2
 
-#define HEXE_EXPECTED_EOL	1
-#define HEXE_INVALID_CHECKSUM	2
-#define HEXE_INVALID_SIZE	3
-#define HEXE_INVALID_TYPE	4
-#define HEXE_MISSING_COLON	5
-#define HEXE_MISSING_EOF	6
-#define HEXE_NOT_HEX		7
-#define HEXE_UNEXPECTED_EOF	8
-#define HEXE_IO_ERROR		9
+#define IHRE_EXPECTED_EOL	1
+#define IHRE_INVALID_CHECKSUM	2
+#define IHRE_INVALID_SIZE	3
+#define IHRE_INVALID_TYPE	4
+#define IHRE_MISSING_COLON	5
+#define IHRE_MISSING_EOF	6
+#define IHRE_NOT_HEX		7
+#define IHRE_UNEXPECTED_EOF	8
+#define IHRE_IO_ERROR		9
 
-#define HEXR_DATA		0x00
-#define HEXR_END_OF_FILE	0x01
-#define HEXR_EXT_SEG_ADDR	0x02
-#define HEXR_START_SEG_ADDR	0x03
-#define HEXR_EXT_LIN_ADDR	0x04
-#define HEXR_START_LIN_ADDR	0x05
+#define IHRR_DATA		0x00
+#define IHRR_END_OF_FILE	0x01
+#define IHRR_EXT_SEG_ADDR	0x02
+#define IHRR_START_SEG_ADDR	0x03
+#define IHRR_EXT_LIN_ADDR	0x04
+#define IHRR_START_LIN_ADDR	0x05
 
-struct hex_record {
-	HEX_U8 type;
-	HEX_U8 size;
-	HEX_U16 addr;
+struct ihr_record {
+	IHR_U8 type;
+	IHR_U8 size;
+	IHR_U16 addr;
 	union {
-		HEX_U8 *data;
-		HEX_U16 base_addr;
-		HEX_U32 ext_instr_ptr;
+		IHR_U8 *data;
+		IHR_U16 base_addr;
+		IHR_U32 ext_instr_ptr;
 		struct {
-			HEX_U16 code_seg;
-			HEX_U16 instr_ptr;
+			IHR_U16 code_seg;
+			IHR_U16 instr_ptr;
 		} start;
 	} data;
 };
 
-int hex_read(int type, FILE *from, struct hex_record *rec);
+int ihr_read(int type, FILE *from, struct ihr_record *rec);
 
-const char *hex_errstr(int errnum);
+const char *ihr_errstr(int errnum);
 
-#endif /* HEX_INCLUDED */
+#endif /* IHR_INCLUDED */
