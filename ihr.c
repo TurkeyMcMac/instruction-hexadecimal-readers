1a45911620ade10eb1a2032548a41f27a7656674
TurkeyMcMac <jwmhjwmh@gmail.com>
2019-01-27 13:36:59 -0500

    Move some functions into sub-blocks of ihr_read
    I did this for a few reasons:
     1. The functions were only used once.
     2. The functions depend on the order in which they are called.
     3. It was annoying having to jump around in the file to read stuff.

--- ihr.c
+++ ihr.c
@@ -43,82 +43,6 @@ static int invalid_hex_error(const char *pair)
 	}
 }
 
-static int read_data(struct ihr_record *rec, const char *hex, size_t *idx)
-{
-	IHR_U8 i;
-	int min_size;
-	switch (rec->type) {
-		case IHRR_EXT_SEG_ADDR:
-		case IHRR_EXT_LIN_ADDR:
-			min_size = 2;
-			break;
-		case IHRR_START_SEG_ADDR:
-		case IHRR_START_LIN_ADDR:
-			min_size = 4;
-			break;
-		default:
-			min_size = -1;
-	}
-	if ((int)rec->size < min_size) {
-		rec->type = -IHRE_INVALID_SIZE;
-		*idx = 1;
-		return -1;
-	}
-	for (i = 0; i < rec->size; ++i) {
-		const char *pair = hex + i * 2;
-		int byte = read_u8(pair);
-		if (byte < 0) {
-			rec->type = invalid_hex_error(pair);
-			*idx += i * 2;
-			return -1;
-		}
-		rec->data.data[i] = byte;
-	}
-	*idx += i * 2;
-	return 0;
-}
-
-static void unionize_data(struct ihr_record *rec)
-{
-	IHR_U8 *data = rec->data.data;
-	switch (rec->type) {
-	case IHRR_DATA:
-	case IHRR_END_OF_FILE:
-		break;
-	case IHRR_EXT_SEG_ADDR:
-	case IHRR_EXT_LIN_ADDR:
-		rec->data.base_addr = ((IHR_U16)data[0] << 8)
-			| (IHR_U16)data[1];
-		break;
-	case IHRR_START_SEG_ADDR:
-		rec->data.start.code_seg = ((IHR_U16)data[0] << 8)
-			| (IHR_U16)data[1];
-		rec->data.start.instr_ptr = ((IHR_U16)data[2] << 8)
-			| (IHR_U16)data[3];
-		break;
-	case IHRR_START_LIN_ADDR:
-		rec->data.ext_instr_ptr = ((IHR_U32)data[0] << 24)
-			| ((IHR_U32)data[1] << 16) | ((IHR_U32)data[2] << 8)
-			| (IHR_U32)data[3];
-		break;
-	}
-}
-
-
-static IHR_U8 calc_checksum(const struct ihr_record *rec)
-{
-	IHR_U8 checksum = 0;
-	IHR_U8 i = 0;
-	checksum += rec->size;
-	checksum += rec->addr >> 8;
-	checksum += rec->addr & 0xFF;
-	checksum += rec->type;
-	for (i = 0; i < rec->size; ++i) {
-		checksum += rec->data.data[i];
-	}
-	checksum = ~checksum + 1;
-	return checksum & 0xFF;
-}
 
 int ihr_read(int file_type,
 	size_t len,
@@ -126,7 +50,7 @@ int ihr_read(int file_type,
 	struct ihr_record *rec)
 {
 	size_t idx = 0;
-	int size, addr, type, checksum;
+	int size, addr, type, read_cksum;
 	if (len < IHR_MIN_LENGTH) {
 		rec->type = -IHRE_SUB_MIN_LENGTH;
 		goto error;
@@ -157,9 +81,42 @@ int ihr_read(int file_type,
 	}
 	idx += 2;
 	if (len < idx + (size + 1) * 2) goto error_invalid_size;
-	if (read_data(rec, text + idx, &idx) < 0) goto error;
-	checksum = read_u8(text + idx);
-	if (checksum < 0) {
+	/* Read data field: */
+	{
+		IHR_U8 i;
+		int min_size;
+		const char *hex;
+		switch (rec->type) {
+			case IHRR_EXT_SEG_ADDR:
+			case IHRR_EXT_LIN_ADDR:
+				min_size = 2;
+				break;
+			case IHRR_START_SEG_ADDR:
+			case IHRR_START_LIN_ADDR:
+				min_size = 4;
+				break;
+			default:
+				min_size = -1;
+		}
+		if ((int)rec->size < min_size) {
+			rec->type = -IHRE_INVALID_SIZE;
+			goto error_invalid_size;
+		}
+		hex = text + idx;
+		for (i = 0; i < rec->size; ++i) {
+			const char *pair = hex + i * 2;
+			int byte = read_u8(pair);
+			if (byte < 0) {
+				rec->type = invalid_hex_error(pair);
+				idx += i * 2;
+				goto error;
+			}
+			rec->data.data[i] = byte;
+		}
+		idx += (size_t)i * 2;
+	}
+	read_cksum = read_u8(text + idx);
+	if (read_cksum < 0) {
 		rec->type = invalid_hex_error(text + idx);
 		goto error;
 	}
@@ -183,11 +140,48 @@ int ihr_read(int file_type,
 		}
 		++idx;
 	}
-	if (checksum != calc_checksum(rec)) {
-		rec->type = -IHRE_INVALID_CHECKSUM;
-		goto error;
+	/* Verify checksum: */
+	{
+		IHR_U8 right_cksum = 0;
+		IHR_U8 i = 0;
+		right_cksum += rec->size;
+		right_cksum += rec->addr >> 8;
+		right_cksum += rec->addr & 0xFF;
+		right_cksum += rec->type;
+		for (i = 0; i < rec->size; ++i) {
+			right_cksum += rec->data.data[i];
+		}
+		right_cksum = (~right_cksum + 1) & 0xFF;
+		if ((IHR_U8)read_cksum != right_cksum) {
+			rec->type = -IHRE_INVALID_CHECKSUM;
+			goto error;
+		}
 	}
-	unionize_data(rec);
+	/* Transfer data from rec->data.data to record-type-specific fields: */
+	{
+		IHR_U8 *data = rec->data.data;
+		switch (rec->type) {
+		case IHRR_DATA:
+		case IHRR_END_OF_FILE:
+			break;
+		case IHRR_EXT_SEG_ADDR:
+		case IHRR_EXT_LIN_ADDR:
+			rec->data.base_addr = ((IHR_U16)data[0] << 8)
+				| (IHR_U16)data[1];
+			break;
+		case IHRR_START_SEG_ADDR:
+			rec->data.start.code_seg = ((IHR_U16)data[0] << 8)
+				| (IHR_U16)data[1];
+			rec->data.start.instr_ptr = ((IHR_U16)data[2] << 8)
+				| (IHR_U16)data[3];
+			break;
+		case IHRR_START_LIN_ADDR:
+			rec->data.ext_instr_ptr = ((IHR_U32)data[0] << 24)
+				| ((IHR_U32)data[1] << 16)
+				| ((IHR_U32)data[2] << 8) | (IHR_U32)data[3];
+			break;
+		}
+	}
 	return idx;
 
 error_invalid_size:
